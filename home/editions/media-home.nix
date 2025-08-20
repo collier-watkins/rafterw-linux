9497cbe380eee6241076142bfa27233839c077e2
Collier Watkins <pwatkins@px.ftw>
2025-08-20 13:51:50 -0500

    Switching to rofi

--- home/editions/media-home.nix
+++ home/editions/media-home.nix
@@ -1,41 +1,40 @@
 { config, pkgs, lib, ... }:
-{
-	imports = [
-		./dev-home.nix
-	];
 
-	home.packages = with pkgs; [
-		rofi
-    	papirus-icon-theme
-	];
-
-
-	# Symlink Papirus to ~/.icons for fuzzel
-	home.file.".icons/Papirus".source = "${pkgs.papirus-icon-theme}/share/icons/Papirus";
-
+let
   rofiTheme = pkgs.writeText "rofi-theme" ''
-* {
-    background-color: #282a36;
-    foreground-color: #f8f8f2;
-}
-'';
- programs.rofi = {
+    * {
+      background-color: #282a36;
+      foreground-color: #f8f8f2;
+    }
+  '';
+in {
+  imports = [
+    ./dev-home.nix
+  ];
+
+  home.packages = with pkgs; [
+    rofi
+    papirus-icon-theme
+  ];
+
+  # Symlink Papirus to ~/.icons for rofi
+  home.file.".icons/Papirus".source = "${pkgs.papirus-icon-theme}/share/icons/Papirus";
+
+  programs.rofi = {
     enable = true;
-	theme = rofiTheme;
+    theme = rofiTheme;
     extraConfig = {
-		show-icons = true;
-		icon-theme = "Papirus";
-	};
+      show-icons = true;
+      icon-theme = "Papirus";
+    };
   };
 
+  wayland.windowManager.sway.config.menu = lib.mkForce "rofi -show drun -theme ${rofiTheme}";
 
-	wayland.windowManager.sway.config.menu = lib.mkForce "rofi -show drun -theme ${rofiTheme}";
+  # If you want custom desktop entries for rofi:
+  # home.file.".local/share/applications/custom".source = ../configs/media-edition-desktop-apps;
 
-    # Symlink your repo directory of .desktop files
-  #home.file.".local/share/fuzzel-apps".source = ../configs/media-edition-desktop-apps;
-
-  # Prepend it so it overrides system entries
   home.sessionVariables = {
-    XDG_DATA_DIRS = "${config.home.homeDirectory}/.local/share/fuzzel-apps:${config.xdg.dataHome}:${pkgs.xdg-utils}/share";
+    XDG_DATA_DIRS = "${config.home.homeDirectory}/.local/share/applications/custom:${config.xdg.dataHome}:${pkgs.xdg-utils}/share";
   };
 }
