a5775b8506376dfe9462103ed9f0560a4546edf9
Collier Watkins <pwatkins@px.ftw>
2025-08-20 12:52:41 -0500

    Moving fuzzel to media

--- home/editions/media-home.nix
+++ home/editions/media-home.nix
@@ -1,17 +1,51 @@
 { config, pkgs, lib, ... }:
-
+let
+	fuzzelDesktopPaths = [
+		"/run/current-system/sw/share/applications"
+		"~/.nix-profile/share/applications"
+	];
+in
 {
 	imports = [
 		./dev-home.nix
 	];
 
 	home.packages = with pkgs; [
-		vlc
-		mpv
-	#	fuzzel
+		fuzzel
 	];
 
-	#wayland.windowManager.sway.config.menu = lib.mkForce "fuzzel";
+
+	# Symlink Papirus to ~/.icons for fuzzel
+	home.file.".icons/Papirus".source = "${pkgs.papirus-icon-theme}/share/icons/Papirus";
+
+	  # Fuzzel wrapper script to ensure correct GTK env
+  home.file.".config/fuzzel/run-fuzzel.sh" = {
+    text = ''
+      #!/usr/bin/env bash
+      export GTK_THEME=Adwaita-dark
+      export GTK_ICON_THEME=Papirus
+      export XDG_DATA_DIRS="$HOME/.nix-profile/share:/run/current-system/sw/share:$XDG_DATA_DIRS"
+      exec fuzzel --show drun
+    '';
+    executable = true;
+  };
+
+	# Fuzzel configuration
+ home.file.".config/fuzzel/config" = {
+    text = ''
+      [launcher]
+      show-icons = true
+      icon-size = 32
+      desktop-files-path = ${lib.concatStringsSep ":" fuzzelDesktopPaths}
+      max-items = 50
+
+      [appearance]
+      background-color = #1e1e2e
+      foreground-color = #FF00FF
+    '';
+  };
+
+	wayland.windowManager.sway.config.menu = lib.mkForce "$HOME/.config/fuzzel/run-fuzzel.sh";
 
     # Symlink your repo directory of .desktop files
   #home.file.".local/share/fuzzel-apps".source = ../configs/media-edition-desktop-apps;
