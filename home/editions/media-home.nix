4ad21a1b27a0e375a8555c35060c17d80b67ead9
Collier Watkins <collier.watkins.chc@gmail.com>
2025-08-20 16:19:50 -0500

    Rofi now shows desired icons

--- home/editions/media-home.nix
+++ home/editions/media-home.nix
@@ -5,8 +5,10 @@
   ];
 
   home.packages = with pkgs; [
-    rofi
+    rofi # Unmodified rofi package
     papirus-icon-theme
+    desktop-file-utils
+    mlocate
   ];
 
   # Symlink Papirus to ~/.icons for rofi
@@ -14,22 +16,35 @@
   # Deploy your standalone theme file into ~/.config/rofi/theme.rasi
   home.file.".config/rofi/theme.rasi".source = ../configs/media/rofi/theme.rasi;
 
+  # Override system .desktop files to hide Rofi and Rofi Theme Selector
+  home.file.".local/share/applications/rofi.desktop".text = ''
+    [Desktop Entry]
+    Type=Application
+    Name=Rofi
+    NoDisplay=true
+  '';
+  home.file.".local/share/applications/rofi-theme-selector.desktop".text = ''
+    [Desktop Entry]
+    Type=Application
+    Name=Rofi Theme Selector
+    NoDisplay=true
+  '';
+
   programs.rofi = {
     enable = true;
     extraConfig = {
       show-icons = true;
       icon-theme = "Papirus";
-      # Add filter to exclude Rofi Theme Selector
-      #drun-match = "!Rofi Theme Selector";
+      display-drun = "Applications:";
     };
   };
 
   wayland.windowManager.sway.config.menu = lib.mkForce "rofi -show drun -theme ~/.config/rofi/theme.rasi";
 
-  # Custom .desktop files
-  home.file.".local/share/rofi/desktop-items".source = ../configs/media/desktop-items;
+  # Custom .desktop files in applications/ subdirectory
+  home.file.".local/share/rofi/desktop-items/applications".source = ../configs/media/desktop-items;
 
   home.sessionVariables = {
-    XDG_DATA_DIRS = "${config.home.homeDirectory}/.local/share/rofi/desktop-items";
+    XDG_DATA_DIRS = "${config.home.homeDirectory}/.local/share/rofi/desktop-items:${config.home.homeDirectory}/.local/share/applications";
   };
 }
