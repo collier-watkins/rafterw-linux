c06b4e94998c6de9b0dc1d9ccbee0671781f5536
Collier Watkins <pwatkins@px.ftw>
2025-08-20 14:08:55 -0500

    Switching to rofi

--- home/editions/media-home.nix
+++ home/editions/media-home.nix
@@ -1,5 +1,13 @@
 { config, pkgs, lib, ... }:
 
+let
+  rofiTheme = pkgs.writeText "rofi-theme.rasi" ''
+    * {
+      background-color: #282a36;
+      foreground-color: #f8f8f2;
+    }
+  '';
+in
 {
   imports = [
     ./dev-home.nix
@@ -15,24 +23,19 @@
 
   programs.rofi = {
     enable = true;
-    theme = ''
-      * {
-        background-color: #282a36;
-        foreground-color: #f8f8f2;
-      }
-    ''; # ✅ properly close here
     extraConfig = {
       show-icons = true;
       icon-theme = "Papirus";
     };
   };
 
-  wayland.windowManager.sway.config.menu = lib.mkForce "rofi -show drun";
+  wayland.windowManager.sway.config.menu = lib.mkForce "rofi -show drun -theme ${rofiTheme}";
 
   # Custom .desktop files
   home.file.".local/share/applications/custom".source = ../configs/media-edition-desktop-apps;
 
   home.sessionVariables = {
-    XDG_DATA_DIRS = "${config.home.homeDirectory}/.local/share/applications/custom:${config.xdg.dataHome}:${pkgs.xdg-utils}/share";
+    XDG_DATA_DIRS =
+      "${config.home.homeDirectory}/.local/share/applications/custom:${config.xdg.dataHome}:${pkgs.xdg-utils}/share";
   };
 }
